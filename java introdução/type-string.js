b974cfccfa7afd71474a95592a5d037b550132c3
BiaCEEP <126881092+BiaCEEP@users.noreply.github.com>
2023-09-15 08:47:19 -0300

    Add files via upload

--- java introdução/type-string.js
+++ java introdução/type-string.js
@@ -3,11 +3,11 @@ const texto2 = 'Olá mundo!';
 const senha  = "senhaSuperSegura456!"
 const StringDeNumeros = "34567";
 
-const citacao = "O Leo disse 'oi!'";
-console.log(citacao)
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
 
 // coscatenação (+)
 
-
+console.log(citacao + meuNome)
 
 //template string OU template literal 
